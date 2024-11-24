59f05fcabac8871667990150590bcc0292235968
kemad929 <kemad@cvbtt.com>
2024-11-24 17:01:23 +0300

    structured format

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -6,21 +6,34 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
 
 public class BaseTest {
 	
+	private final By txt_username_web = By.name("username");
+	private final By txt_password_web = By.id("password");
+	private final By btn_login_web = By.xpath("//button[contains(text(),'Login')]");
+	private final By Home_Button = By.xpath("//*[@id=\"collapsibleNavbar\"]/ul/li[1]/a");
 	public static SHAFT.GUI.WebDriver driver;
 	public homepage Homepage ;
 
-
+	public  void login (String loginname , String password) {
+		
+		driver.element().type(txt_username_web,  loginname);
+		driver.element().type(txt_password_web,  password);
+		driver.element().click(btn_login_web);
+		driver.element().assertThat(Home_Button).isVisible();
+	}
+	@Parameters({"loginname", "password"})
 		@BeforeClass(alwaysRun = true)
-		public void setup() {
+		public void setup(String loginname , String password) {
 		    driver = new SHAFT.GUI.WebDriver();
 		    driver.browser().navigateToURL("https://siteassured.cvbtt.com/");
 		    Homepage = new homepage(driver);
+		    login(loginname, password);
 		    }
 		
 		@AfterClass(alwaysRun = true)
--- src/test/java/Tests/sign_upTest.java
+++ src/test/java/Tests/sign_upTest.java
@@ -1,16 +1,21 @@
 package Tests;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import Pages.homepage;
+import io.qameta.allure.Step;
 
 public class sign_upTest extends BaseTest{
 	public homepage Homepage ;
+	//@Parameters({"loginname", "password"})
   @Test
   public void admin_loginTest() {
 	  
 	  Homepage = new homepage(driver);
-		Homepage.login();
+	  System.out.println("Opening login page...");
+	//  Homepage.login(loginame, password);
 
   }
+	
 }
